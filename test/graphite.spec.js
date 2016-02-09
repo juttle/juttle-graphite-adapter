c3168f58f479187fb4630f2c6b8f6ecc6fa1ffaa
rodney <rodneygomes@gmailcom>
2016-02-09 14:57:01 -0800

    convert to use new adapter API
    fixes #29
    
    this is a huge overhaul of things to use the new adapter API which also
    gives us the ability to do "live" data reading.
    
    I also piggy backed on this change the move to use eslint and gulp to
    run things

--- test/graphite.spec.js
+++ test/graphite.spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var juttle_test_utils = require('juttle/test/runtime/specs/juttle-test-utils');
 var check_juttle = juttle_test_utils.check_juttle;
 var expect = require('chai').expect;
@@ -55,35 +57,63 @@ describe('graphite-adapter API tests', function () {
         });
     });
 
-    it('reads no points initially', function() {
+    it('handles reading no points with -last', function() {
         return check_juttle({
-            program: 'read graphite -from :5 minutes ago: name="metric.does.not.exist"'
+            program: 'read graphite -last :5 minutes: name="metric.does.not.exist"'
         })
         .then(function(result) {
             expect(result.errors.length).equal(0);
+            expect(result.warnings.length).equal(0);
             expect(result.sinks.table.length).equal(0);
         });
     });
 
-    it('read without -from fails', function() {
+    it('handles reading no points with -from/-to', function() {
         return check_juttle({
-            program: 'read graphite name~"bananas"'
+            program: 'read graphite -from :5 minutes ago: -to :now: name="metric.does.not.exist"'
         })
-        .then(function() {
-            throw Error('Previous statement should have failed');
-        }).catch(function(err) {
-            expect(err.message).to.contain('invalid read graphite required option -from.');
+        .then(function(result) {
+            expect(result.errors.length).equal(0);
+            expect(result.warnings.length).equal(0);
+            expect(result.sinks.table.length).equal(0);
         });
     });
 
-    it('read with -unknown fails', function() {
+    it('fails when provided an unknown option', function() {
         return check_juttle({
-            program: 'read graphite -unknown "bananas"'
+            program: 'read graphite -unknown "bananas" name="metric.*"'
         })
         .then(function() {
             throw Error('Previous statement should have failed');
         }).catch(function(err) {
-            expect(err.message).to.contain('unknown read graphite option unknown.');
+            expect(err.message).to.contain('unknown read-graphite option unknown');
+        });
+    });
+
+    it('can read "live" points', function() {
+        var uniqueness = uuid.v1().substring(0, 6);
+
+        return check_juttle({
+            // pushing the point 2s into the future to simulate live
+            program: 'emit -from :now: -limit 1 ' +
+                '| put name="metric' + uniqueness + '", value = count(), time = time + :2s:  | write graphite'
+        })
+        .then(function(result) {
+            expect(result.errors.length).equal(0);
+            expect(result.warnings.length).equal(0);
+        })
+        .then(function() {
+            return check_juttle({
+                program: 'read graphite -lag :5s: name="metric' + uniqueness + '"',
+                realtime: true
+            }, 10000)
+            .then(function(result) {
+                expect(result.errors.length).equal(0);
+                expect(result.warnings.length).equal(0);
+                expect(result.sinks.table.length).equal(1);
+                expect(result.sinks.table[0].name).equal('metric' + uniqueness);
+                expect(result.sinks.table[0].value).equal(1);
+            });
         });
     });
 
@@ -105,16 +135,17 @@ describe('graphite-adapter API tests', function () {
             return retry(function() {
                 return check_juttle({
                     // -:1s: becuase from is exclusive
-                    program: 'read graphite -from :' + now + ':-:1s: name="metric' + uniqueness + '"'
+                    program: 'read graphite -from :' + now + ':-:1s: -to :now: name="metric' + uniqueness + '"'
                 })
                 .then(function(result) {
                     expect(result.errors.length).equal(0);
+                    expect(result.warnings.length).equal(0);
                     expect(result.sinks.table.length).equal(1);
                     expect(result.sinks.table[0].time).equal(now);
                     expect(result.sinks.table[0].name).equal('metric' + uniqueness);
                     expect(result.sinks.table[0].value).equal(0);
                 });
-            }, { interval:1000, timeout: 5000});
+            }, { interval:250, timeout: 5000});
         });
     });
 
@@ -136,7 +167,7 @@ describe('graphite-adapter API tests', function () {
             return retry(function() {
                 return check_juttle({
                     // -:1s: becuase from is exclusive
-                    program: 'read graphite -from :' + now + ':-:1s: name="metric' + uniqueness + '"'
+                    program: 'read graphite -from :' + now + ':-:1s: -to :now: name="metric' + uniqueness + '"'
                 })
                 .then(function(result) {
                     expect(result.errors.length).equal(0);
@@ -163,7 +194,7 @@ describe('graphite-adapter API tests', function () {
         .then(function() {
             return retry(function() {
                 return check_juttle({
-                    program: 'read graphite -from :2 hours ago: name="metric' + uniqueness + '"'
+                    program: 'read graphite -last :2 hours: name="metric' + uniqueness + '"'
                 })
                 .then(function(result) {
                     expect(result.errors.length).equal(0);
@@ -192,7 +223,7 @@ describe('graphite-adapter API tests', function () {
                 return check_juttle({
                     // 25 hours ago because time is moving and 24 hours ago
                     // from the moment we did the write is now 24 hours ago + a few seconds
-                    program: 'read graphite -from :25 hours ago: name="metric' + uniqueness + '"'
+                    program: 'read graphite -last :25 hours: name="metric' + uniqueness + '"'
                 })
                 .then(function(result) {
                     expect(result.errors.length).equal(0);
@@ -203,7 +234,7 @@ describe('graphite-adapter API tests', function () {
         .then(function() {
             return retry(function() {
                 return check_juttle({
-                    program: 'read graphite -from :20 hours ago: name="metric' + uniqueness + '"'
+                    program: 'read graphite -last :20 hours: name="metric' + uniqueness + '"'
                 })
                 .then(function(result) {
                     expect(result.errors.length).equal(0);
@@ -238,7 +269,7 @@ describe('graphite-adapter API tests', function () {
         .then(function() {
             return retry(function() {
                 return check_juttle({
-                    program: 'read graphite -from :1 minute ago: ' +
+                    program: 'read graphite -last :1 minute: ' +
                         'name~"metric' + uniqueness + '.region2.*"'
                 })
                 .then(function(result) {
